79f73969c58827336c333b941e1f56cce65bf9ed
User <User@LAPTOP-QC69QVEV>
2023-10-16 01:05:43 +0800

    Welcome Java Programming !

---
+++ Multiple.java/src/com/studyopedia/Multiple.java
@@ -0,0 +1,12 @@
+package com.studyopedia;
+
+public class Multiple {
+
+	public static void main(String[] args) {
+	
+			 System.out.println("Welcome");
+		     System.out.println("Java");
+		     System.out.println("Programming !");
+	}
+
+}
